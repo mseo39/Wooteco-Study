6e0d89dc8ceb4c331f3a63ca08810acd5176730f
mseo39 <a01063294905@gmail.com>
2023-12-04 23:34:17 +0900

    feat: 길이 범위 확인하는 기능

--- java-bridge/src/main/java/bridge/Exception.java
+++ java-bridge/src/main/java/bridge/Exception.java
@@ -14,4 +14,11 @@ public class Exception {
             throw new IllegalArgumentException(ERROR + "숫자를 입력해 주세요.");
         }
     }
+
+    public static int checkNumberRange(int input) {
+        if (3 > input || 20 < input) {
+            throw new IllegalArgumentException(ERROR + "다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+        return input;
+    }
 }
