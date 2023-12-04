d200756abfa9a64c765bfd24c45d45b6fc7e49c4
mseo39 <a01063294905@gmail.com>
2023-12-04 23:48:45 +0900

    입력 요청 문구 출력기능

--- java-bridge/src/main/java/bridge/OutputView.java
+++ java-bridge/src/main/java/bridge/OutputView.java
@@ -65,4 +65,8 @@ public class OutputView {
     public static void printSize() {
         System.out.println("다리의 길이를 입력해주세요.");
     }
+
+    public static void printMoveChoice() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
 }
