3360ed804acaa507bdbfeafb0bd70d8c2dd975d8
mseo39 <a01063294905@gmail.com>
2023-12-04 23:48:14 +0900

    feat: 다리길이 입력문구 출력기능

--- java-bridge/src/main/java/bridge/OutputView.java
+++ java-bridge/src/main/java/bridge/OutputView.java
@@ -61,4 +61,8 @@ public class OutputView {
     public static void printStart() {
         System.out.println("다리 건너기 게임을 시작합니다.");
     }
+
+    public static void printSize() {
+        System.out.println("다리의 길이를 입력해주세요.");
+    }
 }
