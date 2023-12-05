c0ed0bd8d66723c827627d30628926181d58a90d
mseo39 <a01063294905@gmail.com>
2023-12-06 01:09:28 +0900

    feat: 피하고 싶은 음식 입력

--- java-menu/src/main/java/menu/view/InputView.java
+++ java-menu/src/main/java/menu/view/InputView.java
@@ -16,4 +16,9 @@ public class InputView {
         System.out.println(Output.COACH_INPUT.getPhrase());
         return Arrays.stream(readLine().split(",")).toList();
     }
+
+    static public List<String> readAvoidMenu(String name){
+        System.out.println(String.format(Output.AVOID_MENU_INPUT.getPhrase(),name));
+        return Arrays.stream(readLine().split(",")).toList();
+    }
 }
