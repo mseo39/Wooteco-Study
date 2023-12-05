53bfc1d43e77fbf7609297e447a1546f25f39b1f
mseo39 <a01063294905@gmail.com>
2023-12-06 01:05:00 +0900

    feat: User 생성

---
+++ java-menu/src/main/java/menu/model/User.java
@@ -0,0 +1,36 @@
+package menu.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private String name;
+    private List<String> avoidMenu;
+    private List<String> alreadyRecommendMenu;
+
+    public User(String name) {
+        this.name = name;
+        this.alreadyRecommendMenu = new ArrayList<>();
+        this.avoidMenu = new ArrayList<>();
+    }
+
+    public List<String> getAlreadyRecommendMenu() {
+        return alreadyRecommendMenu;
+    }
+
+    public List<String> getAvoidEatingMenu() {
+        return avoidMenu;
+    }
+
+    public void addAlreadyRecommendMenu(String menu) {
+        this.alreadyRecommendMenu.add(menu);
+    }
+
+    public void setAvoidMenu(List<String> avoidMenu) {
+        this.avoidMenu = avoidMenu;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
