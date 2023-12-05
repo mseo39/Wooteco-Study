830a5137895f891c5fbfef413a9c78112897f35a
mseo39 <a01063294905@gmail.com>
2023-12-05 15:20:03 +0900

    feat: 숫자로 생성된 다리 다리모양표현 기능

--- java-bridge/src/main/java/bridge/BridgeMaker.java
+++ java-bridge/src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,8 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
@@ -9,5 +12,21 @@ public class BridgeMaker {
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
+    }
+
+    /**
+     * @param size 다리의 길이
+     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
+     */
+    public List<String> makeBridge(int size) {
+        List<String> bridgeList = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            if (bridgeNumberGenerator.generate() == 0) {
+                bridgeList.add("D");
+                continue;
+            }
+            bridgeList.add("U");
+        }
+        return bridgeList;
     }
 }
