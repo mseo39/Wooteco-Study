cf0f468abead13010f4565385fb540836907c6db
mseo39 <a01063294905@gmail.com>
2023-12-13 02:56:32 +0900

    feat: 컴퓨터 모델 생성

---
+++ java-baseball-6-main/src/main/java/baseball/model/Computer.java
@@ -0,0 +1,18 @@
+package baseball.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class Computer {
+    private List<Integer> baseballNum;
+    private int size;
+
+    public List<Integer> getBaseballNum() {
+        return baseballNum;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
