631bb21948e0f84990aff6fdbd541f6579230f5b
mseo39 <a01063294905@gmail.com>
2023-12-13 03:26:46 +0900

    feat: 문ìupdate´ 정수로 변경 수정

--- java-baseball-6-main/src/main/java/baseball/util/Util.java
+++ java-baseball-6-main/src/main/java/baseball/util/Util.java
@@ -3,12 +3,9 @@ package baseball.util;
 import baseball.view.ErrorPhrase;
 
 public class Util {
-    public static int stringToInteger(String input, int min) {
+    public static int stringToInteger(String input) {
         try {
             int i = Integer.parseInt(input);
-            if (i < min) {
-                throw new IllegalArgumentException(ErrorPhrase.BASEBALL_SIZE.getPhrase());
-            }
             return i;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ErrorPhrase.STRING_TO_INTEGER.getPhrase());
