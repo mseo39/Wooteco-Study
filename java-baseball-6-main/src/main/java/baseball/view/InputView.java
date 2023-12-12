d857447485380a76e21a12558f808035c028debe
mseo39 <a01063294905@gmail.com>
2023-12-13 04:09:34 +0900

    feat: 사용자로부터 숫자 입력박기

--- java-baseball-6-main/src/main/java/baseball/view/InputView.java
+++ java-baseball-6-main/src/main/java/baseball/view/InputView.java
@@ -3,11 +3,18 @@ package baseball.view;
 import baseball.util.Util;
 import baseball.util.Validator;
 
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
     public static int readBaseballSize(int min) {
-        System.out.println(InputPhrase.BASEBALL_SIZE);
+        System.out.println(InputPhrase.BASEBALL_SIZE.getPhrase());
         return Validator.baseballSize(Util.stringToInteger(readLine()), min);
+    }
+
+    public static List<Integer> readUserBaseball(){
+        System.out.print(InputPhrase.BASEBALL_INPUT.getPhrase());
+        return Validator.inputDuplicate(readLine());
     }
 }
