70a149cc9c77997506afd965796777470e42d0c5
mseo39 <a01063294905@gmail.com>
2023-12-13 03:32:01 +0900

    feat: 야구 길이 검사 추ì가

--- java-baseball-6-main/src/main/java/baseball/view/InputView.java
+++ java-baseball-6-main/src/main/java/baseball/view/InputView.java
@@ -1,12 +1,13 @@
 package baseball.view;
 
 import baseball.util.Util;
+import baseball.util.Validator;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
-    public static int readBaseballSize() {
+    public static int readBaseballSize(int min) {
         System.out.println(InputPhrase.BASEBALL_SIZE);
-        return Util.stringToInteger(readLine());
+        return Validator.baseballSize(Util.stringToInteger(readLine()), min);
     }
 }
