8e9590b098e47e297a623fb8800b0e4ee807da8e
mseo39 <a01063294905@gmail.com>
2023-12-13 03:54:32 +0900

    feat: baseball 생성

--- java-baseball-6-main/src/main/java/baseball/model/Baseball.java
+++ java-baseball-6-main/src/main/java/baseball/model/Baseball.java
@@ -2,26 +2,31 @@ package baseball.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Baseball {
     private List<Integer> baseballNum;
     private int size;
+    private int minSize=2;
+
+    public Baseball(){
+        baseballNum = new ArrayList<>();
+    }
 
     public List<Integer> getBaseballNum() {
         return baseballNum;
+    }
+
+    public int getMinSize() {
+        return minSize;
     }
 
     public void setSize(int size) {
         this.size = size;
     }
 
-    public void randomBaseballNum(){
-        while (baseballNum.size() < size) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!baseballNum.contains(randomNumber)) {
-                baseballNum.add(randomNumber);
-            }
-        }
+    public int getSize() {
+        return size;
     }
 }
