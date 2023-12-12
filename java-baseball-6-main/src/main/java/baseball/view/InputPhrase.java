c3b6c2f3d26c11cec65c57b1811797a922deba81
mseo39 <a01063294905@gmail.com>
2023-12-13 03:22:05 +0900

    style: 입력 문구

--- java-baseball-6-main/src/main/java/baseball/view/InputPhrase.java
+++ java-baseball-6-main/src/main/java/baseball/view/InputPhrase.java
@@ -2,6 +2,7 @@ package baseball.view;
 
 public enum InputPhrase {
     BASEBALL_INPUT("숫자를 입력해주세요 : "),
+    BASEBALL_SIZE("정답의 길이를 설정하세요. (예시: 123 -> 길이 3)"),
     RESTART_CHOICE("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
     private String phrase;
