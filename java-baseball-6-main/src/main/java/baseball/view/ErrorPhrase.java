63d72c6a5e9d75f4123b0596242cfd677aca364c
mseo39 <a01063294905@gmail.com>
2023-12-13 03:19:50 +0900

    feat: 숫자 길이 입력 추가

--- java-baseball-6-main/src/main/java/baseball/view/ErrorPhrase.java
+++ java-baseball-6-main/src/main/java/baseball/view/ErrorPhrase.java
@@ -4,6 +4,7 @@ public enum ErrorPhrase {
     BASEBALL_SIZE("정답의 길이는 2보다 작을 수 없습니다."),
     RESTART_CHOICE("재시작은 1, 종료는 2를 입력하세요."),
     INPUT_SIZE("입력 숫자의 길이는 %s입니다."),
+    STRING_TO_INTEGER("입력 숫자의 길이는 %s입니다."),
     INPUT_DUPLICATE("입력된 숫자는 중복되면 안됩니다");
     private String phrase;
     private static String error = "[ERROR]";
