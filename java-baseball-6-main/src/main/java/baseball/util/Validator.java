cfa69a02f77c89f686fe13a82c332d1dd9c118b7
mseo39 <a01063294905@gmail.com>
2023-12-13 04:10:04 +0900

    feat: 입력된 숫자 중복 확인

--- java-baseball-6-main/src/main/java/baseball/util/Validator.java
+++ java-baseball-6-main/src/main/java/baseball/util/Validator.java
@@ -1,10 +1,11 @@
 package baseball.util;
 
 import baseball.view.ErrorPhrase;
-import template.view.ErrorPhrase;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Validator {
     public static int baseballSize(int num, int size){
@@ -12,5 +13,12 @@ public class Validator {
             throw new IllegalArgumentException(ErrorPhrase.BASEBALL_SIZE.getPhrase());
         }
         return num;
+    }
+
+    public static List<Integer> inputDuplicate(String input){
+        if(input.length()!=input.chars().distinct().count()){
+            throw new IllegalArgumentException(ErrorPhrase.INPUT_DUPLICATE.getPhrase());
+        }
+        return Stream.of(input.split("")).map(Integer::parseInt).collect(Collectors.toList());
     }
 }
