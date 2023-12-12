309031d0df9938b468fc2ee07983575c3abeacab
mseo39 <a01063294905@gmail.com>
2023-12-13 03:11:45 +0900

    feat: 게임 시작 문구 출력

---
+++ java-baseball-6-main/src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,9 @@
+package baseball.view;
+
+import baseball.view.OutputPhrase;
+
+public class OutputView {
+    public static void printStart(){
+        System.out.println(OutputPhrase.START.getPhrase());
+    }
+}
