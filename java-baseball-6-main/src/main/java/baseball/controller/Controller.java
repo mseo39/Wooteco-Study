fe95805cfa616b9c1c22bb05137c0b63077d69e7
mseo39 <a01063294905@gmail.com>
2023-12-13 04:21:20 +0900

    feat: 사용자로부터 숫자 입력 요청

--- java-baseball-6-main/src/main/java/baseball/controller/Controller.java
+++ java-baseball-6-main/src/main/java/baseball/controller/Controller.java
@@ -1,17 +1,32 @@
 package baseball.controller;
 
 import baseball.model.Baseball;
+import baseball.model.User;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 
 public class Controller {
     Baseball baseball;
+    User user;
     public Controller() {
         baseball = new Baseball();
+        user = new User();
     }
 
     public void startProgram() {
         requestStart();
+        requestInputUser();
+    }
+
+    public void requestInputUser(){
+        while (true) {
+            try {
+                user.addInputBaseball(InputView.readUserBaseball());
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     public void requestStart(){
