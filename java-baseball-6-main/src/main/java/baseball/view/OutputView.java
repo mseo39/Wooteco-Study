7e40e244fe3705a3372fedc9c2408643e81c278a
mseo39 <a01063294905@gmail.com>
2023-12-13 13:51:22 +0900

    feat: 볼 스트라이크 낫싱 결과 출력

--- java-baseball-6-main/src/main/java/baseball/view/OutputView.java
+++ java-baseball-6-main/src/main/java/baseball/view/OutputView.java
@@ -6,4 +6,20 @@ public class OutputView {
     public static void printStart(){
         System.out.println(OutputPhrase.START.getPhrase());
     }
+
+    public static void printCheckResult(boolean nothing, int ball, int strike){
+        if(nothing){
+            System.out.println(OutputPhrase.NOTHING.getPhrase());
+            return;
+        }
+        if(ball==0 && strike!=0){
+            System.out.println(strike+OutputPhrase.STRIKE.getPhrase());
+            return;
+        }
+        if(ball!=0 && strike==0){
+            System.out.println(ball+OutputPhrase.BALL.getPhrase());
+            return;
+        }
+        System.out.println(ball+OutputPhrase.BALL.getPhrase()+" "+strike+OutputPhrase.STRIKE.getPhrase());
+    }
 }
