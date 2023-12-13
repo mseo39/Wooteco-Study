11fd89196183725f363851a1d63fbb4aa6b01cdb
mseo39 <a01063294905@gmail.com>
2023-12-14 00:27:22 +0900

    feat: 자동차 이름 입력 요청

--- java-racingcar-6-main/src/main/java/racingcar/controller/Controller.java
+++ java-racingcar-6-main/src/main/java/racingcar/controller/Controller.java
@@ -1,11 +1,23 @@
 package racingcar.controller;
 
+import racingcar.model.Car;
 import racingcar.model.Game;
+import racingcar.view.InputView;
 
 public class Controller {
     Game game;
 
     public Controller() {
         game = new Game();
+    }
+
+    public void gameStart(){
+        requestCarName();
+    }
+
+    public void requestCarName(){
+        for(String name : InputView.readCarName()){
+            game.addCar(new Car(name));
+        }
     }
 }
