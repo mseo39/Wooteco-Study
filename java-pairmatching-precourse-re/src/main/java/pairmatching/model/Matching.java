2331c02cfb99082fb8bd92e5185a0e49c7124ce0
mseo39 <a01063294905@gmail.com>
2023-12-13 18:20:43 +0900

    feat: ì매칭 모델 생성

---
+++ java-pairmatching-precourse-re/src/main/java/pairmatching/model/Matching.java
@@ -0,0 +1,18 @@
+package pairmatching.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Matching {
+    private Course course;
+    private Level level;
+    private String mission;
+    private List<List<String>> names;
+
+    public Matching(String course, String level, String mission) {
+        this.course = Course.validateCourse(course);
+        this.level = Level.validateLevel(level);
+        this.mission = Level.validateMission(this.level, mission);
+        this.names = new ArrayList<>();
+    }
+}
