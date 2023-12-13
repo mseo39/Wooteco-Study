78b04e5d5e3d72c7f64353b9468da0d3f35e3185
mseo39 <a01063294905@gmail.com>
2023-12-13 19:07:05 +0900

    refactor: 페어매칭 ë모델 필드 자료형 수정

--- java-pairmatching-precourse-re/src/main/java/pairmatching/model/PairMatching.java
+++ java-pairmatching-precourse-re/src/main/java/pairmatching/model/PairMatching.java
@@ -4,10 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PairMatching {
-    private List<List<String>> matchings;
+    private List<Matching> matchings;
     final int tryNum = 3;
 
     public PairMatching(){
         matchings = new ArrayList<>();
+    }
+
+    public List<Matching> getMatchings() {
+        return matchings;
+    }
+
+    public void addMatchings(Matching matching){
+        this.matchings.add(matching);
     }
 }
