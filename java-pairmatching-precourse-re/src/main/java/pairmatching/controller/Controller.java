c1b7930aebb3d30823b5ee962b04d0f33331ae49
mseo39 <a01063294905@gmail.com>
2023-12-13 19:10:02 +0900

    feat: 사용자로부터 매칭 원하는 조유 입력 요청

--- java-pairmatching-precourse-re/src/main/java/pairmatching/controller/Controller.java
+++ java-pairmatching-precourse-re/src/main/java/pairmatching/controller/Controller.java
@@ -2,22 +2,43 @@ package pairmatching.controller;
 
 import pairmatching.model.Course;
 import pairmatching.model.Crew;
+import pairmatching.model.Matching;
+import pairmatching.model.PairMatching;
 import pairmatching.view.InputView;
 import pairmatching.view.OutputView;
 
 import java.nio.file.Paths;
+import java.util.List;
 
 public class Controller {
     private Crew backendCrew;
     private Crew frontendCrew;
+    private PairMatching pairMatching;
 
     public Controller() {
         backendCrew = new Crew(Paths.get("src/main/resources/backend-crew.md"), Course.BACKEND);
         frontendCrew = new Crew(Paths.get("src/main/resources/frontend-crew.md"), Course.FRONTEND);
+        pairMatching = new PairMatching();
     }
 
     public void startProgram() {
-        requestReadFunction();
+        while (requestFunction(requestReadFunction())){
+
+        }
+    }
+
+    public boolean requestFunction(String input){
+        if(input.equals(1)) {
+            requestMatching();
+            return true;
+        }
+        if(input.equals(2)) {
+            return true;
+        }
+        if(input.equals(3)) {
+            return true;
+        }
+        return false;
     }
 
     public String requestReadFunction(){
@@ -28,5 +49,24 @@ public class Controller {
                 System.out.println(e.getMessage());
             }
         }
+    }
+
+    public Matching requestReadMatchingType(){
+        Matching matching;
+        while (true) {
+            try {
+                OutputView.printInformation();
+                List<String> input =InputView.readMatchingType();
+                matching = new Matching(input.get(0), input.get(1),input.get(2));
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return matching;
+    }
+
+    public void requestMatching(){
+        this.pairMatching.addMatchings(requestReadMatchingType());
     }
 }
--- java-pairmatching-precourse-re/src/main/java/pairmatching/view/InputView.java
+++ java-pairmatching-precourse-re/src/main/java/pairmatching/view/InputView.java
@@ -2,11 +2,19 @@ package pairmatching.view;
 
 import pairmatching.util.Validator;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
     public static String readFunction() {
         System.out.println(InputPhrase.CHOICE_FUNCTION.getPhrase());
         return Validator.validateFunction(readLine());
+    }
+
+    public static List<String> readMatchingType(){
+        System.out.println(InputPhrase.CHOICE_MATCHING.getPhrase());
+        return Arrays.stream(readLine().split(",")).map(String::strip).toList();
     }
 }
