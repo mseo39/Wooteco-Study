3984b155677b0863107da4364cbbfda558b5bba1
mseo39 <a01063294905@gmail.com>
2023-12-14 00:04:10 +0900

    feat: 문자열을 정수로 변환하는 기능

---
+++ java-racingcar-6-main/src/main/java/racingcar/util/Util.java
@@ -0,0 +1,17 @@
+package racingcar.util;
+
+import racingcar.view.ErrorPhrase;
+
+public class Util {
+    public static int stringToInteger(String input) {
+        try {
+            int i = Integer.parseInt(input);
+            if (i < 0) {
+                throw new IllegalArgumentException(ErrorPhrase.STRING_TO_INTEGER.getPhrase());
+            }
+            return i;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorPhrase.STRING_TO_INTEGER.getPhrase());
+        }
+    }
+}
