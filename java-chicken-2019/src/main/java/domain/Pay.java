c54abfb065af3cc64622e65548b2dadecafdb0f3
mseo39 <a01063294905@gmail.com>
2023-12-14 18:24:56 +0900

    feat: 치킨종류 수량 10개씩 10000원 할인

--- java-chicken-2019/src/main/java/domain/Pay.java
+++ java-chicken-2019/src/main/java/domain/Pay.java
@@ -5,6 +5,7 @@ public enum Pay {
     CASH(2);
 
     private int num;
+    private final static int DISCOUNT=10000;
 
     Pay(int num) {
         this.num = num;
@@ -14,7 +15,8 @@ public enum Pay {
         return num;
     }
 
-    public int totalMoney(){
-
+    public int discountChicken(Table table, Orders orders){
+        return (orders.TableByChickenNum(table)/10) * DISCOUNT;
     }
+
 }
