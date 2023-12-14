bb7201cbdd863080f59483e0fd38e91fb0b7b7fb
mseo39 <a01063294905@gmail.com>
2023-12-15 01:22:18 +0900

    feat: 입력 문구

--- java-lotto/java-lotto-6-main/src/main/java/lotto/view/InputPhrase.java
+++ java-lotto/java-lotto-6-main/src/main/java/lotto/view/InputPhrase.java
@@ -1,7 +1,10 @@
-package template.view;
+package lotto.view;
 
 public enum InputPhrase {
-    INPUT_CHOICE("");
+    MONEY("구입금액을 입력해 주세요."),
+    WINNING_NUMBER("당첨 번호를 입력해 주세요."),
+    BONUS("보너스 번호를 입력해 주세요.");
+
 
     private String phrase;
 
