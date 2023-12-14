c0d05a9b426b6788d4afd05d226f7e56d0efe171
mseo39 <a01063294905@gmail.com>
2023-12-14 18:35:09 +0900

    feat:컨트롤러 생성

--- java-chicken-2019/src/main/java/controller/Controller.java
+++ java-chicken-2019/src/main/java/controller/Controller.java
@@ -1,11 +1,16 @@
 package controller;
 
+import domain.Orders;
+import org.mockito.internal.matchers.Or;
+
 public class Controller {
+    private Orders orders;
 
     public Controller() {
-
+        orders = new Orders();
     }
 
     public void startProgram() {
 
     }
+}
