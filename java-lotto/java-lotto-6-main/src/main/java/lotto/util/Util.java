bee9043f0266cc42318411663956b5e7f66de46e
mseo39 <a01063294905@gmail.com>
2023-12-15 01:31:27 +0900

    feat: 문자열을 정수로 ë변환

--- java-lotto/java-lotto-6-main/src/main/java/lotto/util/Util.java
+++ java-lotto/java-lotto-6-main/src/main/java/lotto/util/Util.java
@@ -1,15 +1,21 @@
-package template.util;
+package lotto.util;
+
+import lotto.view.ErrorPhrase;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Util {
     public static int stringToInteger(String input) {
         try {
             int i = Integer.parseInt(input);
             if (i < 0) {
-                throw new IllegalArgumentException("");
+                throw new IllegalArgumentException(ErrorPhrase.STRING_TO_INTEGER.getPhrase());
             }
             return i;
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("");
+            throw new IllegalArgumentException(ErrorPhrase.STRING_TO_INTEGER.getPhrase());
         }
     }
 }
