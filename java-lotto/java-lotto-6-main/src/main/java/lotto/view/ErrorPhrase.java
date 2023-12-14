d4ba259a35ebfce926da1908c26dd6adc4b265d5
mseo39 <a01063294905@gmail.com>
2023-12-15 01:20:55 +0900

    feat: 에러문구

--- java-lotto/java-lotto-6-main/src/main/java/lotto/view/ErrorPhrase.java
+++ java-lotto/java-lotto-6-main/src/main/java/lotto/view/ErrorPhrase.java
@@ -1,7 +1,9 @@
-package template.view;
+package lotto.view;
 
 public enum ErrorPhrase {
-    EXIST_MISSION("존재하지 않는 미션입니다.");
+    DIVIDE("구입 금액은 1000원으로 나누어 떨어져야 합니다."),
+    STRING_TO_INTEGER("양수를 입력해야 합니다."),
+    LOTTO_NUM_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
 
     private String phrase;
     private static String error = "[ERROR]";
