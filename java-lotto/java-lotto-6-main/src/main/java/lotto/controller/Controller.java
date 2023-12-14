782992a59707cb4545cff985f810f1c2847e0658
mseo39 <a01063294905@gmail.com>
2023-12-15 03:00:03 +0900

    feat: 구입 금액 입력

--- java-lotto/java-lotto-6-main/src/main/java/lotto/controller/Controller.java
+++ java-lotto/java-lotto-6-main/src/main/java/lotto/controller/Controller.java
@@ -1,6 +1,11 @@
 package template.controller;
 
+import lotto.model.Lottos;
+import lotto.util.Validator;
+import lotto.view.InputView;
+
 public class Controller {
+    private Lottos lottos;
 
     public Controller() {
 
@@ -9,3 +14,8 @@ public class Controller {
     public void startProgram() {
 
     }
+
+    public void requestMoney(){
+        lottos = new Lottos(InputView.readMoney()/1000);
+    }
+}
