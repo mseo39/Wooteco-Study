790b68525935a7fd086123c36418868c2dd6ae2e
mseo39 <a01063294905@gmail.com>
2023-12-14 17:40:26 +0900

    feat: 결제 모델 ã생성

---
+++ java-chicken-2019/src/main/java/domain/Pay.java
@@ -0,0 +1,16 @@
+package domain;
+
+public enum Pay {
+    CARD(1),
+    CASH(2);
+
+    private int num;
+
+    Pay(int num) {
+        this.num = num;
+    }
+
+    public int getNum() {
+        return num;
+    }
+}
